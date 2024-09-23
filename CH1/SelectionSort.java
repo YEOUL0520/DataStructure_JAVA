c9489e69fa1ef931a719c2e4b0b605593a832d19
YEOUL <92625845+YEOUL0520@users.noreply.github.com>
2024-09-23 22:35:14 +0900

    1st edit

--- CH1/SelectionSort.java
+++ CH1/SelectionSort.java
@@ -1,11 +1,11 @@
 public class SelectionSort {
     public void sort(int[] A){
-        for (int i = 0; i < A.length -1; i++){
-            int min = i;
-            for (int k = i+1; k <A.length; k++)
-                if (A[k] < A[min])
-                    min = k;
-            int temp = A[i];
+        for (int i = 0; i < A.length -1; i++){ //배열 인덱스 0부터 끝에서 두번째까지(맨 끝은 비교 필요x)
+            int min = i; //min값의 인덱스를 i로 설정
+            for (int k = i+1; k <A.length; k++) //배열 인덱스 1부터 마지막까지 비교
+                if (A[k] < A[min]) //k값이 더 작을 경우
+                    min = k; //min인덱스를 k로 지정
+            int temp = A[i]; //min값과 i값을 swap (제일 작은 값이 맨 앞으로 오게 됨)
             A[i] = A[min];
             A[min] = temp;
         }
